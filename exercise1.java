0b53ce8b3f9015eca8abcbaca6795fb49833cd7c
meganmcnultyyy <58346271+meganmcnultyyy@users.noreply.github.com>
2020-04-28 14:07:24 +0100

    exercise1.java

---
+++ exercise1.java
@@ -0,0 +1,17 @@
+//Megan McNulty 20th April 2020
+// SOftware Developement 1 - Project
+//Exercise 1
+
+public class exercise1{
+  public static void main(String[] args){
+	String name = "Mary";
+	int age = 20;
+	double weight = 65.48;
+	String colour = "red";
+
+	System.out.println(name+ " is " +age+ " years old.");
+	System.out.println("She weighs " +weight+ " kilograms.");
+	System.out.println("Her favourite colour is "+colour);
+
+   }
+}
