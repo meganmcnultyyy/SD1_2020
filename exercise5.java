d1ba931af3dcde2613fc9c7dbff18b4876924d6a
meganmcnultyyy <58346271+meganmcnultyyy@users.noreply.github.com>
2020-04-28 16:57:52 +0100

    Add files via upload

---
+++ exercise5.java
@@ -0,0 +1,17 @@
+//Megan McNulty 20th April 2020
+//Software Developement 1 - Project
+//Exercise 5
+
+import java.util.Scanner;
+
+public class exercise5{
+  public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	int [] x = {20, 30, 40, 50};
+
+	    System.out.println("Sum Total: "+(x[0]+x[1]+x[2]+x[3]));
+	    System.out.println("Sum Product: "+(x[0]*x[1]*x[2]*x[3]));
+	    System.out.println("Sum Average: "+((x[0]+x[1]+x[2]+x[3])/4));
+
+	}
+}
