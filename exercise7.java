5dde5d58e4a266a6fa1061b4d517163f144ae746
meganmcnultyyy <58346271+meganmcnultyyy@users.noreply.github.com>
2020-04-28 17:26:23 +0100

    Update exercise7.java

--- exercise7.java
+++ exercise7.java
@@ -5,6 +5,7 @@
 import java.util.Random object;
 
 public class exercise7{
+	/*Got this code from the brief for the project*/
   public int[] reorganize(int [] numbers){
 	  Random rnd = new Random();
 	  for (int i = numbers.length - 1; i >= 0; i--){
